00ef2a69fbd00fde36d1c688f7863686e69ce9b5
Taehyun Lee <etaehyun4@gmail.com>
2020-06-12 23:38:16 +0900

    Add two extract pattern algorithms
    1. Using All subarray in particular length range
    2. Using All LCS of 2 user combinations

--- locust/extract_patterns.py
+++ locust/extract_patterns.py
@@ -11,6 +11,9 @@ class API(object):
     def __str__(self):
         return f"Idx: {self.idx}, Method: {self.method}, URI: {self.uri}"
 
+    def __eq__(self, rhs):
+        return self.method == rhs.method and self.uri == rhs.uri
+
 class APIManager(object):
     api_map = {}
     idx_map = {}
@@ -26,19 +29,12 @@ class APIManager(object):
         if idx in self.idx_map: return self.idx_map[uri]
         return None
 
-    def lookup(s):
-        for pattern, value in API_MAP:
-            if re.search(pattern, s):
-                return value
-        return None
-
     def find_by_uri(self, method, uri):
         for key in self.api_map:
             m, u = key
             if method == m and re.search(u, uri):
                 return self.api_map[key]
-
-        return None
+        return API(None, method, uri)
 
 APIS = [
     (1, 'GET', '^/main/$'),
@@ -85,6 +81,71 @@ def validate(line):
     d = json.loads(line)
     return not d['URI'].endswith('.css') and not d['URI'].endswith('.js') and not 'media' in d['URI']
 
+def extract_pattern1(user_logs, min_length = 10, max_length = 20):
+    candidates = []
+    keys = user_logs.keys()
+    for key in keys:
+        logs = user_logs[key]
+        if len(logs) < min_length: continue
+        for length in range(min_length, max_length + 1):
+            p = []
+            for api in logs:
+                p.append((api.method, api.uri))
+                if len(p) == length:
+                    candidates.append(tuple(p))
+                    p.pop(0)
+
+    return sorted(dict(Counter(candidates)).items(), key=lambda item: (item[1], len(item[0])), reverse=True)
+
+def getLCS(log1, log2):
+    N = len(log1)
+    M = len(log2)
+
+    D = [[-1] * (M + 1)]
+    P = [[0] * (M + 1)]
+    for i in range(N):
+        D.append([0] * (M + 1))
+        P.append([0] * (M + 1))
+        for j in range(M):
+            if log1[i] == log2[j] and D[i + 1][j + 1] < D[i][j] + 1:
+                D[i + 1][j + 1] = D[i][j] + 1
+                P[i + 1][j + 1] = 1
+            if D[i + 1][j + 1] < D[i + 1][j]:
+                D[i + 1][j + 1] = D[i + 1][j]
+                P[i + 1][j + 1] = 2
+            if D[i + 1][j + 1] < D[i][j + 1]:
+                D[i + 1][j + 1] = D[i][j + 1]
+                P[i + 1][j + 1] = 3
+
+    i = N
+    j = M
+    lcs = []
+    while i > 0 and j > 0:
+        if P[i][j] == 1:
+            lcs = [(log1[i - 1].method, log1[i - 1].uri)] + lcs
+            i -= 1
+            j -= 1
+        elif P[i][j] == 2:
+            j -= 1
+        elif P[i][j] == 3:
+            i -= 1
+        else:
+            break
+
+    return lcs
+
+def extract_pattern2(user_logs):
+    candidates = []
+    keys = list(user_logs.keys())
+    N = len(keys)
+    for i in range(N):
+        for j in range(i+1, N):
+            result = (tuple(getLCS(user_logs[keys[i]], user_logs[keys[j]])))
+            if len(result) > 1:
+                candidates.append(result)
+
+    return sorted(dict(Counter(candidates)).items(), key=lambda item: (item[1], len(item[0])), reverse=True)
+
 if __name__ == '__main__':
     init()
 
@@ -102,5 +163,3 @@ if __name__ == '__main__':
         key = log['cookie']['csrftoken']
         api = API_MANAGER.find_by_uri(log['method'], log['URI'])
         if api: user_logs[key].append(api)
-    
-
